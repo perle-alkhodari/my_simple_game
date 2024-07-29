33bc7e977119bf36e25e8bdd2473d3c570ccee20
perle-alkhodari <perle.alkhodari@gmail.com>
2024-07-29 23:13:37 +0300

    2 (probably final) changes:
    - updated the color of the "player" (square).
    - put all the arguments going into the square render method into a struct called player.

--- my_simple_game/game.cpp
+++ my_simple_game/game.cpp
@@ -1,35 +1,37 @@
 #define BACKGROUND_COLOR 0x237458
 
+struct Player {
+	float xPos = 50;
+	float yPos = 50;
+	float xSize = 60;
+	float ySize = 60;
+	float color = 0xF0C900;
+} player;
+
 bool isDown(int inputButton);
 bool isPressed(int inputButton);
 bool isReleased(int inputButton);
-
 void simulate_game(Input* input);
 
-float xPos = 50;
-float yPos = 50;
-float xSize = 60;
-float ySize = 60;
+
 void simulate_game(Input* input)
 {
 	ClearScreen(BACKGROUND_COLOR);
 
 	if (isPressed(BUTTON_UP)) {
-		yPos += 1;
+		player.yPos += 1;
 	}
 	if (isPressed(BUTTON_LEFT)) {
-		xPos -= 1;
+		player.xPos -= 1;
 	}
 	if (isPressed(BUTTON_DOWN)) {
-		yPos -= 1;
+		player.yPos -= 1;
 	}
 	if (isPressed(BUTTON_RIGHT)) {
-		xPos += 1;
+		player.xPos += 1;
 	}
-	DrawRectDynamicPosAndSize(xPos, yPos, xSize, ySize, 0x000000);
+	DrawRectDynamicPosAndSize(player.xPos, player.yPos, player.xSize, player.ySize, player.color);
 }
-
-
 
 
 bool isDown(int inputButton)
